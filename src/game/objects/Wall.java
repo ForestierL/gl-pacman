7517124289f8a669ee05d0f8e07e8be923bf38a8
baronialexandre <cyril.roubaud@gmail.com>
2019-11-27 19:42:32 +0100

    Merge branch 'master' of https://github.com/ForestierL/gl-pacman

--- src/game/objects/Wall.java
+++ src/game/objects/Wall.java
@@ -6,10 +6,14 @@ import javafx.scene.image.Image;
 
 public class Wall extends GameObject
 {
-    public Wall(int x, int y, int width, int height)
+    public Wall(int x, int y, int width, int height, int textureId)
     {
         super(new SpriteTexture(new Image("mapTileset.png")), x, y, width, height);
-        setDefaultSubImage(7);
+        setDefaultSubImage(textureId);
+    }
+
+    public  Wall(int x, int y, int width, int height) {
+        this(x,y,width,height,7);
     }
 
     @Override
